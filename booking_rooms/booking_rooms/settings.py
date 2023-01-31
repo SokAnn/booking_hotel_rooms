674e96b198de964216c8049ac18cdeb1d9cae694
Ann <fullsokan@gmail.com>
2023-01-31 23:56:43 +0300

    last

--- booking_rooms/booking_rooms/settings.py
+++ booking_rooms/booking_rooms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z4=$h)atq6&a*0=(3*o$6mnyx9#46%t40zbc&zu8gvd!=_*f)o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
